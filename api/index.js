afa9395f59d98dceb7fbb95ca3b3ef948f280f79
welsonla <wyc.jar@gmail.com>
2023-08-10 00:48:26 +0800

    增加公共接口访问

---
+++ api/index.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const host = "http://127.0.0.1:5000"
+
+function request(method, route, obj, domain = '') {
+  let url =`${host}/${route}`
+  return new Promise(function (resolve, reject) {
+    axios({
+      method: method.toLocaleString(),
+      url,
+      data: obj || {}
+    }).then(function (response) {
+      console.log(`${response}`)
+      resolve(response.data, url)
+    }).catch(function (error) {
+      reject(error);
+    });
+  });
+}
+
+//  登录
+export const login = (params = {}) => request(`post`, '/api/login', params)
+
+// 注册
+export const register = (params = {}) => request(`post`, '/api/register', params)
--- pages/login.vue
+++ pages/login.vue
@@ -23,27 +23,27 @@
         <template v-if="selectIndex===0">
           <!-- 登录表单 -->
           <div class="flex flex-col">
-              <label for="username" class="nav-text-color leading-8 text-sm">用户名或邮箱</label>
-              <input type="text" name="username" placeholder="用户名/邮箱" class="h-10 border-color rounded-md border text-sm pl-2"/>
+              <label for="username" class="nav-text-color leading-8 text-sm" >用户名或邮箱</label>
+              <input type="text" name="username" placeholder="用户名/邮箱" class="h-10 border-color rounded-md border text-sm pl-2" v-model="loginName"/>
               <label for="password" class="nav-text-color leading-8 text-sm mt-2">密码</label>
-              <input type="password" name="password" placeholder="密码" class="h-10 border-color rounded-md border text-sm pl-2"/>
+              <input type="password" name="password" placeholder="密码" class="h-10 border-color rounded-md border text-sm pl-2" v-model="loginPass"/>
           </div>
-          <button type="submit" class="h-8 bg-green-600 rounded-md text-white mt-4">登录</button>
+          <button type="submit" class="h-8 bg-green-600 rounded-md text-white mt-4" @click="login">登录</button>
           <!-- End 登录表单 -->
         </template>
         <template v-else>
           <!-- 注册表单 -->
           <div class="flex flex-col">
               <label for="username" class="nav-text-color leading-8 text-sm">用户名</label>
-              <input type="text" name="username" placeholder="用户名" class="h-10 border-color rounded-md border text-sm pl-2"/>
+              <input type="text" name="username" placeholder="用户名" class="h-10 border-color rounded-md border text-sm pl-2" v-model="regName"/>
               <label for="email" class="nav-text-color leading-8 text-sm mt-2">邮箱</label>
-              <input type="text" name="email" placeholder="邮箱" class="h-10 border-color rounded-md border text-sm pl-2"/>
+              <input type="text" name="email" placeholder="邮箱" class="h-10 border-color rounded-md border text-sm pl-2" v-model="regEmail"/>
               <label for="password" class="nav-text-color leading-8 text-sm mt-2">密码</label>
-              <input type="password" name="password" placeholder="密码" class="h-10 border-color rounded-md border text-sm pl-2"/>
+              <input type="password" name="password" placeholder="密码" class="h-10 border-color rounded-md border text-sm pl-2" v-model="regPass"/>
               <label for="password-confirm" class="nav-text-color leading-8 text-sm mt-2">确认密码</label>
-              <input type="password" name="password-confirm" placeholder="确认密码" class="h-10 border-color rounded-md border text-sm pl-2"/>
+              <input type="password" name="password-confirm" placeholder="确认密码" class="h-10 border-color rounded-md border text-sm pl-2" v-model="regPassConfirm"/>
           </div>
-          <button type="submit" class="h-8 bg-green-600 rounded-md text-white mt-4">注册</button>
+          <button type="submit" class="h-8 bg-green-600 rounded-md text-white mt-4" @click="register">注册</button>
           <!-- End 注册表单 -->
         </template>
         <!-- End 登录注册Form -->
@@ -54,17 +54,61 @@
 
 <script>
 import Layout from "@/pages/layout";
+import * as api from '@/api';
+
 export default {
   name: "login",
   components: {Layout},
   methods:{
     updateTab(index){
       this.selectIndex = index
+    },
+    login() {
+      let params = {
+        username: this.loginName,
+        password: this.loginPass
+      }
+      api.login(params).then((res) => {
+
+      }).catch((e) => {
+
+      })
+    },
+    register() {
+      if (this.regName.length == 0) {
+        alert('请输入用户名')
+        return
+      }
+
+      if (this.regEmail.length == 0) {
+        alert('请输入注册邮箱')
+        return;
+      }
+      if (this.regPass !== this.regPassConfirm) {
+        alert('两次密码不匹配')
+        return
+      }
+      let params = {
+        username:this.regName,
+        email:this.regEmail,
+        password:this.regPass
+      }
+      api.register(params).then((resp) => {
+
+      }).catch((e) => {
+
+      })
     }
   },
   data(){
     return {
-      selectIndex: 1
+      selectIndex: 0,
+      loginName:"",
+      loginPass:"",
+      regName:"",
+      regEmail:"",
+      regPass:"",
+      regPassConfirm:""
     }
   }
 }
