7fa2e3bba1511a9192e8b950ae3f2ec40d21fa2d
welsonla <wyc.jar@gmail.com>
2023-09-13 18:41:14 +0800

    增加书评页面编辑器

--- pages/book/review/create.vue
+++ pages/book/review/create.vue
@@ -1,9 +1,10 @@
 <template>
 <Layout>
   <div class="flex flex-col my-0 mx-auto review-form">
+    <div class="nav-text-color text-2xl font-medium mb-2">写书评</div>
   <!-- 图书信息 -->
   <div class="book-info flex flex-row">
-    <img src="~/assets/images/s2885632.jpg" class="book-cover">
+    <img src="http://127.0.0.1:5000/static/images/book/s3745215.jpg" class="book-cover">
     <div class="flex flex-col justify-around ml-2">
       <div class="text-blue-400">明朝那些事儿</div>
       <div class="text-xs text-gray-400">限量版/当年明月</div>
@@ -25,8 +26,18 @@
   <!-- 编写内容 -->
   <div class="flex flex-col write-form ">
     <input type="text" class="review-title" placeholder="添加标题" v-model="title">
-    <textarea rows="10" cols="8" class="mt-4 review-content" v-model="content">
-    </textarea>
+    <!-- <textarea rows="10" cols="8" class="mt-4 review-content" v-model="content">
+    </textarea> -->
+    <div class="editor-container mt-2">
+          <div class="quill-editor"
+               :content="comment"
+               @change="onEditorChange($event)"
+               @blur="onEditorBlur($event)"
+               @focus="onEditorFocus($event)"
+               @ready="onEditorReady($event)"
+               v-quill:myQuillEditor="editorOption">
+          </div>
+        </div>
     <input type="button" class="submit-button" value="提交" @click="submit" />
   </div>
   <!-- End 编写内容 -->
@@ -61,10 +72,23 @@ export default {
       }).catch((e) => {
         console.log(`create.review.error:${e}`)
       })
+    },
+    onEditorBlur(editor) {
+      console.log('editor blur!', editor)
+    },
+    onEditorFocus(editor) {
+      console.log('editor focus!', editor)
+    },
+    onEditorReady(editor) {
+      console.log('editor ready!', editor)
+    },
+    onEditorChange({ editor, html, text }) {
+      console.log('editor change!', editor, html, text)
+      this.content = html
     }
   },
   mounted() {
-    document.getElementsByClassName("vue-star-rating-rating-text").hide()
+    // document.getElementsByClassName("vue-star-rating-rating-text").hide()
   },
   data() {
     return {
@@ -77,6 +101,16 @@ export default {
         emptyStarColor: '#737373',
         starWidth: 30,
         starHeight: 30
+      },
+      editorOption: {
+        // some quill options
+        placeholder: "发表对这本书的看法...",
+        modules: {
+          toolbar: [
+            ['bold', 'italic', 'underline', 'strike'],
+            ['blockquote', 'code-block']
+          ]
+        }
       }
     }
   }
@@ -94,5 +128,20 @@ export default {
   object-fit: fill;
   height: 80px;
   width: 60px;
+}
+
+
+.editor-container {
+  padding: 0 0 40px 0;
+  background-color: white;
+  .ql-toolbar{
+    background-color: white;
+  }
+  .quill-editor {
+    min-height: 200px;
+    max-height: 400px;
+    overflow-y: auto;
+    background-color: white;
+  }
 }
 </style>
